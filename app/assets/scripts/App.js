6e3d8f01da8a612ad3112d95c7a2a80a9d7afcd5
Tawin <nugitrangsan@gmail.com>
2018-08-02 17:24:41 -0700

    Created MobileMenu object.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,9 @@
 import Timeline from './modules/Timeline';
 import Contact from './modules/Contact';
 import StickyHeader from './modules/StickyHeader';
+import MobileMenu from './modules/MobileMenu';
 
 const timeline = new Timeline();
 const contact = new Contact();
 const stickyHeader = new StickyHeader();
+const mobileMenu = new MobileMenu();
